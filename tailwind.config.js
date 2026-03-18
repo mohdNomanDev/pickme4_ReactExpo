fed35fac9172307c61eaebf6b08372c1cd0a8789
Mohd Noman <mohdnoman0302@gmail.com>
2026-03-19 01:28:18 +0530

    updated tailwind.config.js to include all major directories (app, components, features, hooks, services, store, utils)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ module.exports = {
   darkMode: "class",
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: [
-    "./App.tsx",
-    "./components/**/*.{js,jsx,ts,tsx}",
     "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./features/**/*.{js,jsx,ts,tsx}",
+    "./hooks/**/*.{js,jsx,ts,tsx}",
+    "./services/**/*.{js,jsx,ts,tsx}",
+    "./store/**/*.{js,jsx,ts,tsx}",
+    "./utils/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
