c88785ddc95d1efa2dbac19d404b98fa95556f9d
Mohd Noman <mohdnoman0302@gmail.com>
2026-03-23 10:31:32 +0530

    connected your new FoodHome screen to the root of your application!

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,6 @@
-import { ScrollView, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import ThemeToggle from "@/components/common/ThemeToggle";
-import { LanguageToggle } from "@/components/common/LanguageToggle";
-import RestaurantCardList from "@/components/restaurant/RestaurantCardList";
+import React from "react";
+import FoodHome from "./FoodHome";
 
 export default function Home() {
-  return (
-    <SafeAreaView className="flex-1 bg-background dark:bg-background-dark">
-      {/* Header Bar */}
-      <View className="bg-card dark:bg-card-dark shadow-sm z-10 w-full">
-        <View className="py-4 px-6 md:px-8 max-w-7xl mx-auto w-full flex-row justify-between items-center">
-          <ThemeToggle />
-          <LanguageToggle />
-        </View>
-      </View>
-
-      <View className="flex-1">
-        <RestaurantCardList />
-      </View>
-    </SafeAreaView>
-  );
+  return <FoodHome />;
 }
