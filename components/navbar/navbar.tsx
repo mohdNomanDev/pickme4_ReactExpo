7ffa4cd6ccb3984e0a805cdd49de2be84e5252e9
Mohd Noman <mohdnoman0302@gmail.com>
2026-03-23 09:15:12 +0530

    navbar structure added

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View } from "react-native";
+
+const Navbar = () => {
+  return (
+    <View>
+      {/* Left Section */}
+      <View>{}</View>
+
+      {/* Center Section */}
+      <View>{}</View>
+
+      {/* Right Section */}
+      <View>{}</View>
+    </View>
+  );
+};
+
+export default Navbar;
