dfea49a4ba4282676fcca9c33dd8e4e878d62c5d
Mohd Noman <mohdnoman0302@gmail.com>
2026-03-23 09:52:47 +0530

    created the FoodHome page structure

---
+++ app/FoodHome/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+export default function FoodHome() {
+  return (
+    <View>
+      {/* Top Navigation Area */}
+      <View>
+      </View>
+
+      <ScrollView>
+        {/* Banner / Offers Carousel Section */}
+        <View>
+        </View>
+
+        {/* Quick Categories Section */}
+        <View>
+        </View>
+
+        {/* Featured Restaurants Section */}
+        <View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
